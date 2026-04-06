8f11f4e8f39778b6c9a2a2d26e05c8f426c89d92
pqSatoupq <mawkob511@gmail.com>
2026-04-06 16:29:03 +0900

    Update inference.py

--- inference.py
+++ inference.py
@@ -26,7 +26,7 @@ de_model = DebertaPureRegressor(DEBERTA_PATH).to(DEVICE)
 sf_path = os.path.join(DEBERTA_PATH, "model.safetensors")
 if os.path.exists(sf_path):
     de_model.load_state_dict(load_file(sf_path, device=DEVICE))
-    print("✅ Loaded 'fix4' trauma weights.")
+    print("✅ Loaded DeBERTa weights.")
 de_model = de_model.to(torch.float32).eval()
 de_tokenizer = DebertaV2Tokenizer.from_pretrained(DEBERTA_PATH)
 
@@ -39,9 +39,11 @@ llama_model = AutoModelForCausalLM.from_pretrained(
     LLAMA_PATH, quantization_config=bnb_config, 
     torch_dtype=torch.bfloat16, device_map="auto"
 )
+print("✅✅ Loaded Llama weights.")
 llama_tokenizer = AutoTokenizer.from_pretrained(LLAMA_PATH)
 
 print("🔗 Loading Projector...")
 projector = LlamaEmotionalProjector(llama_dim=3072).to(DEVICE).to(torch.bfloat16)
 projector.load_state_dict(torch.load(PROJECTOR_WEIGHTS, map_location=DEVICE))
+print("✅✅✅ Complete load model")
 projector.eval()
