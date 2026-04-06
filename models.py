6a79ca22364f23834b4b4b03654c59ef6f30847d
pqSatoupq <mawkob511@gmail.com>
2026-04-06 16:29:07 +0900

    Update models.py

--- models.py
+++ models.py
@@ -8,10 +8,18 @@ class DebertaPureRegressor(nn.Module):
         self.config = DebertaV2Config.from_pretrained(model_path)
         self.deberta = DebertaV2Model.from_pretrained(model_path, config=self.config)
         self.regression_head = nn.Sequential(
-            nn.Linear(self.config.hidden_size, 256), nn.ReLU(), nn.Dropout(0.1),
-            nn.Linear(256, 64), nn.ReLU(), 
-            nn.Linear(64, 3),
-            nn.Tanh()
+            # nn.Linear(self.config.hidden_size, 256), nn.ReLU(), nn.Dropout(0.1),
+            # nn.Linear(256, 64), nn.ReLU(), 
+            # nn.Linear(64, 3),
+            # nn.Tanh()
+            
+            nn.Linear(self.config.hidden_size, 512),
+            nn.LayerNorm(512),
+            nn.GELU(),
+            nn.Dropout(0.1),
+            nn.Linear(512, 256),
+            nn.GELU(),
+            nn.Linear(256, 3)
         )
         
     def forward(self, input_ids, attention_mask):
